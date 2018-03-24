aa62516dce7091b6fb603a950daa8bc9e353bb59
alexnm <alex.n.moldovan@gmail.com>
2018-03-24 09:41:46 +0200

    render to string

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 import React from "react";
+import { renderToString } from "react-dom/server";
 import Layout from "./components/Layout";
 
 const app = express();
@@ -9,7 +10,8 @@ const app = express();
 app.use( express.static( path.resolve( __dirname, "../dist" ) ) );
 
 app.get( "/*", ( req, res ) => {
-    const reactDom = ( <Layout /> );
+    const jsx = ( <Layout /> );
+    const reactDom = renderToString( jsx );
 
     res.writeHead( 200, { "Content-Type": "text/html" } );
     res.end( htmlTemplate( reactDom ) );
