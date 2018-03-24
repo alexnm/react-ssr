955c053818af7f74cb90301e8dc80b87183394cb
alexnm <alex.n.moldovan@gmail.com>
2018-03-24 09:48:52 +0200

    hydrate on the client

--- src/client.js
+++ src/client.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
 
 const app = document.getElementById( "app" );
-ReactDOM.render( <Layout />, app );
+ReactDOM.hydrate( <Layout />, app );
