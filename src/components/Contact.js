d2fcbac03149d5ad842ba4ed832ad1624320a788
alexnm <alex.n.moldovan@gmail.com>
2018-03-25 20:12:59 +0300

    adding react-helmet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Helmet from "react-helmet";
 
 const Contact = () => (
-    <h2>This is the contact page</h2>
+    <div>
+        <h2>This is the contact page</h2>
+        <Helmet>
+            <title>Contact Page</title>
+            <meta name="description" content="This is a proof of concept for React SSR" />
+        </Helmet>
+    </div>
 );
 
 export default Contact;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter, matchPath } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
+import Helmet from "react-helmet";
 import routes from "./routes";
 import Layout from "./components/Layout";
 import createStore, { initializeSession } from "./store";
@@ -35,22 +36,24 @@ app.get( "/*", ( req, res ) => {
             </ReduxProvider>
         );
         const reactDom = renderToString( jsx );
-
         const reduxState = store.getState( );
+        const helmetData = Helmet.renderStatic( );
 
         res.writeHead( 200, { "Content-Type": "text/html" } );
-        res.end( htmlTemplate( reactDom, reduxState ) );
+        res.end( htmlTemplate( reactDom, reduxState, helmetData ) );
     } );
 } );
 
 app.listen( 2048 );
 
-function htmlTemplate( reactDom, reduxState ) {
+function htmlTemplate( reactDom, reduxState, helmetData ) {
     return `
         <!DOCTYPE html>
         <html>
         <head>
             <meta charset="utf-8">
+            ${ helmetData.title.toString( ) }
+            ${ helmetData.meta.toString( ) }
             <title>React SSR</title>
         </head>
         
